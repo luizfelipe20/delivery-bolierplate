e6b71ff46016cd84d04522f69894bbde8433f342
luizfelipe20 <felipekjs36@gmail.com>
2020-12-30 14:42:38 -0300

    deploy digital ocean - part 27

--- delivery/settings.py
+++ delivery/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = os.path.join(BASE_DIR, "/static/")
+STATIC_URL = "/static/"
 
 AUTH_USER_MODEL = 'platform_delivery.User'
 

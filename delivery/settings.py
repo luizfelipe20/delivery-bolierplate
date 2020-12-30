c866b98e041f97e4fbee2656ec16e68f1553afb7
luizfelipe20 <felipekjs36@gmail.com>
2020-12-30 11:57:49 -0300

    deploy digital ocean - part 17

--- delivery/settings.py
+++ delivery/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/usr/src/app/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, '/usr/src/app/static/') 
 

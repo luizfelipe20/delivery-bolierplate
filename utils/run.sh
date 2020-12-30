fe9186e8fa4230e4db7b6eb8309ba44ca9bfc241
luizfelipe20 <felipekjs36@gmail.com>
2020-12-30 14:43:06 -0300

    deploy digital ocean - part 28

--- utils/run.sh
+++ utils/run.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-python -m pip install --upgrade pip
-python -m pip install --upgrade Pillow
 python manage.py collectstatic --noinput
 python manage.py migrate
 gunicorn delivery.wsgi --bind=0.0.0.0:80

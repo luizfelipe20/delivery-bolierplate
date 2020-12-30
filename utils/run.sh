5ac6d8fa86a8f4e12d7286abf202a7911c1f2bf1
luizfelipe20 <felipekjs36@gmail.com>
2020-12-30 11:29:51 -0300

    deploy digital ocean - part 10

--- utils/run.sh
+++ utils/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+python -m pip install --upgrade pip
+python -m pip install --upgrade Pillow
 python manage.py collectstatic --noinput
 python manage.py migrate
 gunicorn django_project.wsgi --bind=0.0.0.0:80

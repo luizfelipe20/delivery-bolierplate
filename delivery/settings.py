20c9db164ee6dd37d9edc5c0952fe74c1d77201e
luizfelipe20 <felipekjs36@gmail.com>
2020-12-30 11:54:03 -0300

    deploy digital ocean - part 16

--- delivery/settings.py
+++ delivery/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/') 
+STATIC_ROOT = os.path.join(BASE_DIR, '/usr/src/app/static/') 
 
 AUTH_USER_MODEL = 'platform_delivery.User'
 
